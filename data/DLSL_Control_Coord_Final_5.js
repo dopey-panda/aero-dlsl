50a2ba030c554aa41d3c9a96f5ed0e240c32f88e
dopey-panda <65345132+dopey-panda@users.noreply.github.com>
2020-06-23 12:51:42 +0800

    Add files via upload

---
+++ data/DLSL_Control_Coord_Final_5.js
@@ -0,0 +1,11 @@
+var json_DLSL_Control_Coord_Final_5 = {
+"type": "FeatureCollection",
+"name": "DLSL_Control_Coord_Final_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Point ID": "S1-01", "Date": "6\/17\/2020", "Code": "T-03", "North": 1542209.019, "East": 299865.531, "Height": 330.012, "RMSH": 0.009, "RMSV": 0.024, "H of anten": 1.5, "Elevation": 379.791, "Status": "Fixed", "Time": "11:55:58", "Latitude": 13.56350123, "Longitude": 121.0850938, "Dist. to B": 142.34, "Ground ell": 378.291 }, "geometry": { "type": "Point", "coordinates": [ 121.147487366234799, 13.943061642763007 ] } },
+{ "type": "Feature", "properties": { "Point ID": "S1-Base", "Date": "6\/17\/2020", "Code": "T-01", "North": 1542190.463, "East": 299724.406, "Height": 349.061, "RMSH": 0.009, "RMSV": 0.023, "H of anten": 1.5, "Elevation": 398.84, "Status": "Fixed", "Time": "11:57:43", "Latitude": 13.56343728, "Longitude": 121.0846242, "Dist. to B": 0.0, "Ground ell": 397.34 }, "geometry": { "type": "Point", "coordinates": [ 121.146182889213136, 13.942884003778003 ] } },
+{ "type": "Feature", "properties": { "Point ID": "S2-64", "Date": "6\/17\/2020", "Code": "T-02", "North": 1542213.925, "East": 299752.825, "Height": 348.841, "RMSH": 0.005, "RMSV": 0.008, "H of anten": 1.5, "Elevation": 398.62, "Status": "Fixed", "Time": "19:11:00", "Latitude": 13.56351433, "Longitude": 121.0847183, "Dist. to B": 36.852, "Ground ell": 397.12 }, "geometry": { "type": "Point", "coordinates": [ 121.146444154370215, 13.943098039297743 ] } },
+{ "type": "Feature", "properties": { "Point ID": "S2-65", "Date": "6\/17\/2020", "Code": "T-04", "North": 1542204.482, "East": 299724.64, "Height": 348.849, "RMSH": 0.004, "RMSV": 0.007, "H of anten": 1.5, "Elevation": 398.628, "Status": "Fixed", "Time": "19:16:25", "Latitude": 13.5634829, "Longitude": 121.0846246, "Dist. to B": 14.022, "Ground ell": 397.128 }, "geometry": { "type": "Point", "coordinates": [ 121.146184042741083, 13.943010713881764 ] } }
+]
+}
---
+++ data/DLSL_LiDAR_Calib_FP_8.js
@@ -0,0 +1,8 @@
+var json_DLSL_LiDAR_Calib_FP_8 = {
+"type": "FeatureCollection",
+"name": "DLSL_LiDAR_Calib_FP_8",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Untitled Path", "descriptio": null, "timestamp": null, "begin": null, "end": null, "altitudeMo": null, "tessellate": "1", "extrude": "0", "visibility": "-1", "drawOrder": null, "icon": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 121.143742932922706, 13.943117666767121 ], [ 121.1444928210705, 13.941764458252718 ], [ 121.144556609795501, 13.941796981229752 ], [ 121.143815065993493, 13.943144457789082 ], [ 121.145527671863704, 13.94404933135549 ], [ 121.1463956628684, 13.942610052358377 ], [ 121.146466584975599, 13.942652503971038 ], [ 121.1456053031538, 13.94410237228891 ], [ 121.146804956601315, 13.943831898998679 ], [ 121.143725347784994, 13.942175841245973 ], [ 121.143754982641795, 13.94210949158229 ], [ 121.146835104483998, 13.943754989040546 ] ] ] } }
+]
+}
